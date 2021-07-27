66172747328b33a591ea4e4fcbb902cb823b91e0
Jack Cheng <chengxianga2008@yahoo.com>
2021-07-28 01:54:07 +1000

    unit testing

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -1,15 +1,28 @@
 # -*- coding: utf-8 -*-
 
-from .context import app
+from .context import core
 
 import unittest
+import pandas as pd
 
 
 class AdvancedTestSuite(unittest.TestCase):
     """Advanced test cases."""
 
-    def test_thoughts(self):
-        self.assertIsNone(app.daily_summary())
+    def test_import_data(self):
+        self.assertIsInstance(core.import_data("data/Input.txt"), pd.DataFrame)
+
+    def test_process_data(self):
+        d = {'CLIENT TYPE': ['CL', 'CL'], 'CLIENT NUMBER': ['1234', '1234'], 'ACCOUNT NUMBER': [
+            '0002', '0002'], 'SUBACCOUNT NUMBER': ['0001', '0001'], 'EXCHANGE CODE': ['SGX', 'SGX'], 'PRODUCT GROUP CODE': ['', ''],
+            'SYMBOL': ['FUNK', 'FUNK'], 'EXPIRATION DATE': ['20100910', '20100910'], 'QUANTITY LONG': ['3', '0'], 'QUANTITY SHORT': ['0', '4']}
+        df = pd.DataFrame(data=d)
+        df = core.process_data(df)
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertEqual(len(df.index), 1)
+        self.assertEqual(len(df.index), 1)
+        self.assertEqual(df.iloc[0]['Total_Transaction_Amount'], -1)
+        
 
 
 if __name__ == '__main__':
